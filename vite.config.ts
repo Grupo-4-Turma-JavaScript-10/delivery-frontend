43d564b5af790a1713dcd14ab04d8512f0cde3e3
PatriciaSouza <patriciasdr@hotmail.com>
2026-01-27 14:34:29 -0300

    chore: ajusta base do Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/delivery-frontend/',
   plugins: [
     react(),
     tailwindcss(),
-
   ],
 })
